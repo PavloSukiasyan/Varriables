6d1b2ee7b0cf3beb6d650cc0a7545ca3a9e17e5a
PavloSukiasyan <pavlo.sukiasyan@gmail.com>
2018-02-15 22:03:32 +0200

    First commit For varriables

---
+++ src/Varriables.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : Varriables.cpp
+// Author      : Pavlo Sukiasyan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int numberCats = 5;
+	int numberDogs = 7;
+	int numberAnimals = numberCats + numberDogs;
+
+	cout << "Number of cats: " << numberCats << endl;
+	cout << "Number of Dogs: " << numberDogs << endl;
+
+	cout << "Total: " << numberAnimals << endl;
+
+	cout << "New dog added " << endl;
+	numberDogs = numberDogs + 1;
+	cout <<"New number of Dogs: " << numberDogs << endl;
+	return 0;
+}
